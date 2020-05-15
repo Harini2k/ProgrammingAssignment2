26588e43417f0ce824497f1a2efe41b765c4e06e
Harini2k <64187513+Harini2k@users.noreply.github.com>
2020-05-15 16:59:27 +0530

    Final assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
+## set function inputs the matrix and stores it and inverse in a parent environment,
+## get function retreives the matrix
+## setinv function calcutates the inverse and is retreived by getinv 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv<-NULL
+  set<-function(y) ##set matrix
+  {
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function()x ##get matrix
+  setinv<-function(solve) inv<<-solve  ##set inverse
+  getinv<-function() inv
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
+##If the inverse is already in cache, it returns that. ELse, the inverse is calculated and returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv<- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv<- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
